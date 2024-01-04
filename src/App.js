97978fa9c167815da1f30721bd313d765bb7aace
lndsilva <lndsilva@hotmail.com>
2024-01-04 17:00:39 -0300

    Trabalhando com listas e chaves (Keys)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {StyleSheet, SafeAreaView} from 'react-native';
 
-import ContadorV2 from './components/contador/ContadorV2';
+import ListaProdutos from './components/produtos/ListaProdutos';
+// import UsuarioLogado from './components/UsuarioLogado';
+// import Familia from './components/relacao/Familia';
+// import Membro from './components/relacao/Membro';
+// import ParImpar from './components/ParImpar';
+// import Diferenciar from './components/Diferenciar';
+// import ContadorV2 from './components/contador/ContadorV2';
 // import Pai from './components/indireta/Pai';
 //import Pai from './components/direta';
 //import Contador from './components/Contador';
@@ -13,9 +19,25 @@ import ContadorV2 from './components/contador/ContadorV2';
 // import Primeiro from './components/Primeiro';
 
 export default () => (
-  <View style={style.App}>
-    <ContadorV2 />
+  <SafeAreaView style={style.App}>
+    <ListaProdutos />
     {/* 
+    <UsuarioLogado usuario={{nome:'Maria', email:'maria@hotmail.com'}}/>
+    <UsuarioLogado usuario={{nome:'Maria'}}/>
+    <UsuarioLogado usuario={{email:'maria@hotmail.com'}}/>
+    <UsuarioLogado usuario={null}/>
+    <UsuarioLogado usuario={{}}/>
+    <Familia>
+      <Membro nome='José' sobrenome='Pereira'/>      
+      <Membro nome='Silvia' sobrenome='Pereira'/>      
+    </Familia>
+    <Familia>
+      <Membro nome='Maria' sobrenome='Silva'/>      
+      <Membro nome='Antônio' sobrenome='Silva'/>      
+    </Familia>
+    <ParImpar num={3} />
+    <Diferenciar />
+    <ContadorV2/>
     <Pai />
     <Pai />    
     <Contador inicial={100} passo={13} />
@@ -34,7 +56,7 @@ export default () => (
     <Comp1 />
     <Comp2 />
     <Primeiro /> */}
-  </View>
+  </SafeAreaView>
 );
 
 const style = StyleSheet.create({
---
+++ src/components/produtos/ListaProdutos.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Estilo from '../estilo';
+import produtos from './produtos';
+
+export default props => {
+  function obterLista() {
+    return produtos.map(p => {
+      return (
+        <Text key={p.id}>
+          {p.id}) {p.nome} tem o preço R$ {p.preco}
+        </Text>
+      );
+    });
+  }
+
+  return (
+    <>
+      <Text style={Estilo.txtG}>Lista Produtos</Text>
+      {obterLista()}
+    </>
+  );
+};
